2786880f4577a3fc7f6c6a8df05391a23967015d
suha jin <89185836+djm07073@users.noreply.github.com>
2023-10-28 16:52:00 +0900

    mission2-end

--- src/components/SwapNavigator.tsx
+++ src/components/SwapNavigator.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import { ROUTER02 } from "../config/address";
+import { UniswapV2Router02__factory } from "../typechain";
 
 export default function SwapNavigator() {
   //TODO: getAmountOut function
-  const getAmountOut = () => {};
+  const getAmountOut = async (path: `0x${string}`[], amountIn: bigint) => {
+    const router = UniswapV2Router02__factory.connect(ROUTER02);
+    const result: bigint[] = await router.getAmountsOut(amountIn, path);
+    return result[result.length - 1];
+  };
   return <div></div>;
 }
