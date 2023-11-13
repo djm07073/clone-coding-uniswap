cd9ae069188fe92a68de6db9b901be730bb555e4
suha jin <89185836+djm07073@users.noreply.github.com>
2023-11-14 01:56:04 +0900

    ui 수정

--- src/components/LiquidityProvider.tsx
+++ src/components/LiquidityProvider.tsx
@@ -9,46 +9,45 @@ export default function LiquidityProvider() {
     const [token1, setToken1] = useState<TokenData>();
     const handleAmount0 = (e: React.ChangeEvent<HTMLInputElement>) => { 
         setAmount0Value(e.target.value);
-        // TODO MISSION 6
+        // TODO MISSION 6: setAmount1 Value
     };
     const handleAmount1 = (e: React.ChangeEvent<HTMLInputElement>) => {
       setAmount1Value(e.target.value);
-      // TODO MISSION 6
+      // TODO MISSION 6: setAmount0 Value
     };
     const addLiquidity = () => {
-      // TODO MISSION 6
+      // TODO MISSION 6: 
     };
     
     return (
-      <div>
-        <div className="flex flex-row">
-          <div className="w-1/2 pr-2">
-            <TokenSelect
-              setSelectedToken={setToken0}
-              selectedToken={token0}
-              blockSelectedToken={token1}
-            />
-            <input
-              type="text"
-              className="w-full border rounded px-3 py-2"
-              onChange={handleAmount0}
-              value={amount0Value}
-            />
-          </div>
-          <div className="w-1/2 pl-2">
-            <TokenSelect
-              setSelectedToken={setToken1}
-              selectedToken={token1}
-              blockSelectedToken={token0}
-            />
-            <input
-              type="text"
-              className="w-full border rounded px-3 py-2"
-              onChange={handleAmount1}
-              value={amount1Value}
-            />
-          </div>
+      <div className="flex flex-col items-center">
+        <div className="w-1 pr-2 flex flex-row justify-center m-4">
+          <TokenSelect
+            setSelectedToken={setToken0}
+            selectedToken={token0}
+            blockSelectedToken={token1}
+          />
+          <input
+            type="text"
+            className="w-full border rounded px-3 py-2"
+            onChange={handleAmount0}
+            value={amount0Value}
+          />
         </div>
+        <div className="w-1 pl-2 flex flex-row justify-center m-4">
+          <TokenSelect
+            setSelectedToken={setToken1}
+            selectedToken={token1}
+            blockSelectedToken={token0}
+          />
+          <input
+            type="text"
+            className="w-full border rounded px-3 py-2"
+            onChange={handleAmount1}
+            value={amount1Value}
+          />
+        </div>
+
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-40 rounded"
           onClick={addLiquidity}
